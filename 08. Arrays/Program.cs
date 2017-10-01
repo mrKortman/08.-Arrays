46ff56c90612ef9a84cd473309183c4b01310d97
Boy Kortman <b.kortman@leeuwenborgh.nl>
2017-10-01 20:31:34 +0200

    Chapter 8 of the programming course c#/.net (Arrays)

--- 08. Arrays/Program.cs
+++ 08. Arrays/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace _08.Arrays
 {
+    /* Dit programma laat alle studenten uit de kllas op het scherm zien en toont 
+     * vervolgens het aantal leerlingen */
+
     class Program
     {
         static void Main(string[] args)
         {
+            string[] arrNames = {   "Maurits Annema", "Tom Bastian", "Stefan Beckers", "Dennis Engelen", "Julean Hommel",
+                                    "Nando Kools", "David Lei", "Bo Loomans", "Cedric Louis-Guerin", "Jules Paays", "Michael Pisters",
+                                    "Mark Rendes", "Kevin Ung", "Shunui Zheng" };
+
+            Array.Sort(arrNames);
+
+            Console.WriteLine("Overzicht klas 1IMV-A14M");
+            Console.WriteLine("---------------------------------------------------");
+
+            for (int i = 0; i < arrNames.Length; i++)
+            {
+                Console.WriteLine("{0}). {1}", i + 1, arrNames[i]);
+            }
+
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("In deze klas zitten {0} studenten.", arrNames.Length);
+
+            Console.ReadLine();
         }
     }
 }
